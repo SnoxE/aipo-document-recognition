e1476dc98f763da05fc772f4b430d47dcbaf0174
Marta Dychała <mar754dyc@gmail.com>
2024-06-12 20:10:07 +0200

    Restore main.py and implement photo upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# This Python file uses the following encoding: utf-8
+import sys
+
+from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
+from PySide6.QtGui import QPixmap
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_Widget
+
+class Widget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
+        self.ui.uploadButton.clicked.connect(self.upload_photo)
+
+    def upload_photo(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Image File", "",
+                                                  "Images (*.png *.jpg *.bmp);;All Files (*)", options=options)
+        if fileName:
+            pixmap = QPixmap(fileName)
+            self.ui.photoField.setPixmap(pixmap)
+            self.ui.photoField.setScaledContents(True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Widget()
+    widget.show()
+    sys.exit(app.exec())
--- ui_form.py
+++ ui_form.py
@@ -15,29 +15,35 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGraphicsView, QPushButton, QSizePolicy,
-    QTextBrowser, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QSizePolicy,
+    QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
         Widget.resize(1092, 593)
-        self.graphicsView = QGraphicsView(Widget)
-        self.graphicsView.setObjectName(u"graphicsView")
-        self.graphicsView.setGeometry(QRect(30, 120, 511, 271))
-        self.pushButton = QPushButton(Widget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(140, 420, 301, 31))
-        self.textBrowser = QTextBrowser(Widget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(680, 90, 256, 31))
+        self.uploadButton = QPushButton(Widget)
+        self.uploadButton.setObjectName(u"uploadButton")
+        self.uploadButton.setGeometry(QRect(140, 420, 301, 31))
         self.pushButton_2 = QPushButton(Widget)
         self.pushButton_2.setObjectName(u"pushButton_2")
         self.pushButton_2.setGeometry(QRect(680, 420, 301, 31))
         self.pushButton_3 = QPushButton(Widget)
         self.pushButton_3.setObjectName(u"pushButton_3")
         self.pushButton_3.setGeometry(QRect(680, 360, 301, 31))
+        self.photoField = QLabel(Widget)
+        self.photoField.setObjectName(u"photoField")
+        self.photoField.setGeometry(QRect(70, 70, 441, 301))
+        self.photoField.setAutoFillBackground(True)
+        self.photoField.setScaledContents(True)
+        self.photoField.setAlignment(Qt.AlignCenter)
+        self.label_2 = QLabel(Widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(670, 60, 181, 16))
+#if QT_CONFIG(shortcut)
+        self.label_2.setBuddy(self.label_2)
+#endif // QT_CONFIG(shortcut)
 
         self.retranslateUi(Widget)
 
@@ -46,16 +52,10 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.pushButton.setText(QCoreApplication.translate("Widget", u"Wczytaj zdj\u0119cie", None))
-        self.textBrowser.setHtml(QCoreApplication.translate("Widget", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Menu</p></body></html>", None))
+        self.uploadButton.setText(QCoreApplication.translate("Widget", u"Wczytaj zdj\u0119cie", None))
         self.pushButton_2.setText(QCoreApplication.translate("Widget", u"Baza U\u017cytkownik\u00f3w", None))
         self.pushButton_3.setText(QCoreApplication.translate("Widget", u"Pozosta\u0142e dokumenty", None))
+        self.photoField.setText(QCoreApplication.translate("Widget", u"Tutaj pojawi si\u0119 zdj\u0119cie dokumentu", None))
+        self.label_2.setText(QCoreApplication.translate("Widget", u"Menu", None))
     # retranslateUi
 
